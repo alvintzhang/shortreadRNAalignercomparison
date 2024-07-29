df44013e3ac3fff49a9236aafb47f3e958a33f8a
Alvin Zhang <154933163+alvintzhang@users.noreply.github.com>
2024-07-29 13:51:18 -0400

    Update querytable.py

--- querytable.py
+++ querytable.py
@@ -1,62 +1,29 @@
 import pysam
+import os
 import sys
-import random
 
-bamfile = pysam.AlignmentFile("/Users/AlvinZhang2026/bio_data/several_reads2.bam", "rb")
+# Open the BAM file
+bamfile = pysam.AlignmentFile("/Users/AlvinZhang2026/bio_data/several_reads.bam", "rb")
 
-'''
-This code processes and analyzes sequencing data in a BAM file to understand sequencing alignment, interpreting how
-reads map to the reference genome through long read rna sequencing (Minimap2). It then identifies splice sites and
-creates a table with the query positions and reference positions to extract 150 base subreads from. Using short read rna
-sequencing on the subreads, the code then compares the short read data with the original long read data to tests for
-accuracy and precision
-'''
-
-# Enhancing accuracy and precision, this code allows for more thorough studying of gene expression and regulation.
-
-# parseCigar method which reads the given BAM file
-# returns the sequences, CIGAR strings, and whether the left clip is soft or hard (SoH variable)
 def parseCigar(givenBamfile):
-    seq = []  # all the reads in the BAM file
-    SoH = []  # soft or hard of left clip for each read
-    cigar = []  # array of strings with CIGAR strings of all the potential reads
+    seq = []
+    cigar = []
+    SoH = []
 
-    for read in givenBamfile.fetch():  # if there is more than one read in the file
+    for read in givenBamfile.fetch():
         if read.is_secondary:
             continue
-        print("read name: ", read.query_name)
-        print("reference name: ", read.reference_name)
-        print("first position on ref: ", read.reference_start)
-        print("first position on read: ", read.query_alignment_start)
-        print("read sequence: ", read.query_sequence)
-        seq.append(read.query_sequence)
+        seq.append((read.query_name, read.query_sequence))
         cigar.append(read.cigarstring)
 
-        print("cigar: ", read.cigarstring)
-
         for (op, le) in read.cigartuples:
-            if op == 0 or op == 7 or op == 8:
-                print("match: ", le)
-            elif op == 1:
-                print("insertion: ", le)
-            elif op == 2:
-                print("deletion: ", le)
-            elif op == 3:
-                print("intron: ", le)
-            elif op == 4:
-                print("soft clip: ", le)
-                if not SoH:
-                    SoH.append(1)
-            elif op == 5:
-                if not SoH:
-                    SoH.append(2)
-                print("hard clip: ", le)
+            if op == 4 and not SoH:
+                SoH.append(1)
+            elif op == 5 and not SoH:
+                SoH.append(2)
 
     return [seq, cigar, SoH]
 
-
-# Creates two arrays that hold the query position and reference positions of the read
-# Cannot differentiate between deletions and splices
 def QRtable1(givenBamfile):
     query_positions = []
     reference_positions = []
@@ -66,166 +33,105 @@ def QRtable1(givenBamfile):
             query_positions.append(query_pos)
             reference_positions.append(ref_pos)
 
-    array = [query_positions, reference_positions]
-    return array
+    return [query_positions, reference_positions]
 
-'''
-Just like QRtable1 but also returns an array with the splices
-Therefore able to differentiate between all operations returned by cigartuples
-'''
 def QRtable2(givenBamfile, SoH):
     query_positions2 = []
     reference_positions2 = []
-
-    splices = []  # holds last match and first match (beginning/end of splice)
-    # splices holds positions of the query column/row of the table
-
+    splices = []
     splices2 = []
-    SoHindex = 0
-    queryindex = 0
-    referenceindex = 0
 
     for read in givenBamfile.fetch():
-
         if SoH == 1:
             queryindex = read.query_alignment_start
         elif SoH == 2:
             queryindex = 0
         else:
-            print("No clipping Error")
             sys.exit(1)
         referenceindex = read.reference_start
 
         for op, le in read.cigartuples:
-            if op == 0:  # match or mismatch
+            if op == 0:  # Match
                 for i in range(le):
                     query_positions2.append(queryindex)
                     reference_positions2.append(referenceindex)
                     queryindex += 1
                     referenceindex += 1
-            elif op == 1:  # insertion
+            elif op == 1:  # Insertion
                 queryindex += le
-            elif op == 2:  # deletion
+            elif op == 2:  # Deletion
                 referenceindex += le
-            elif op == 3:  # splice
+            elif op == 3:  # Skip
                 splices.append([queryindex - 1, queryindex])
-                splices2.append([[queryindex - 1, referenceindex],[queryindex, referenceindex+le]])
+                splices2.append([[queryindex - 1, referenceindex], [queryindex, referenceindex + le]])
                 query_positions2.append(queryindex)
                 reference_positions2.append(referenceindex)
                 referenceindex += le
 
-    array = [query_positions2, reference_positions2, splices, splices2]
-    return array
+    return [query_positions2, reference_positions2, splices, splices2]
 
-'''
-Because our splices array contains the index of where the splices are based on the query index
-We want to convert from the query index to the reference positions in order to find the actual bases where the splices are
-This function also prints out the results
-'''
 def findRefSplicePos(qp, rp, s):
-    dict = {}
+    ref_dict = {}
     assert len(qp) == len(rp)
     for i in range(len(qp)):
-        dict[qp[i]] = rp[i]
+        ref_dict[qp[i]] = rp[i]
 
-    # printing results
     qrDASplicePos = []
     for x in s:
         qrDASplicePos.append(
-            "{}:{},{}:{}".format(x[0], dict[x[0]], x[1], dict[x[1]]))  # printing out the splices and ref positions
-    # returns a dict with the both the query positions and the reference positions
-    return [dict, qrDASplicePos]
+            "{}:{},{}:{}".format(x[0], ref_dict[x[0]], x[1], ref_dict[x[1]]))
+    return [ref_dict, qrDASplicePos]
 
-
-# Extracts all subreads of the given length from the query sequences
-# Returns a list with the subreads
-def extract_subreads(query_seq, start_positions, length):
-    # Extract non-overlapping subreads from the query sequences
+def extract_subreads(query_seq, start_positions, length, long_read_id, reference_positions):
     subreads = []
     used_positions = set()
 
-    for start_pos in start_positions:
-        # Check if the subread can be extracted without overlap
-        if start_pos not in used_positions and (start_pos + length <= len(query_seq)):
-            subread = query_seq[start_pos:start_pos + length]  # Extract subread of given length
-            subreads.append(subread)
-            # Mark positions as used
+    for i, start_pos in enumerate(start_positions):
+        if start_pos is not None and start_pos not in used_positions and (start_pos + length <= len(query_seq)):
+            subread = query_seq[start_pos:start_pos + length]
+            ref_start = reference_positions[i] if i < len(reference_positions) else None
+            ref_end = reference_positions[i + length - 1] if i + length - 1 < len(reference_positions) else None
+            subread_id = f"{long_read_id}_subread_{i + 1}_start_{start_pos}_end_{start_pos + length - 1}"
+            subreads.append((subread, start_pos, start_pos + length - 1, ref_start, ref_end, long_read_id, subread_id))
             used_positions.update(range(start_pos, start_pos + length))
 
-    # returns the list of extracted subreads
     return subreads
 
-# Extracts subreads randomly (number of subreads extracted depends on the length of the sequence)
-def extract_random_subreads(query_seq, length, query_positions, reference_positions):
-    subreads = []
-    num_elements = len(query_positions) // length
-
-    for _ in range(num_elements):
-        start_pos_index = random.randint(0, len(query_positions) - 1)
-        start_pos = query_positions[start_pos_index]
-        end_pos = start_pos + length - 1
-
-        if end_pos < len(query_seq):
-            subread = query_seq[start_pos:end_pos + 1]
-            query_alignment_start = start_pos
-            query_alignment_end = end_pos + 1
-            reference_start = reference_positions[start_pos_index]
-            reference_end = reference_positions[start_pos_index + length - 1] + 1
-            subreads.append((subread, query_alignment_start, query_alignment_end, start_pos, reference_start, reference_end))
-
-            # Debugging outputs to help identify the position issue
-            print(f"Extracted Subread {len(subreads)}: {subread}")
-            print(f"Query Start: {query_alignment_start}, Query End: {query_alignment_end}")
-            print(f"Reference Start: {reference_start}, Reference End: {reference_end}")
-            print("------")
-
-    return subreads
-
-# comparison method
 def compare_subreads(subreads, short_read_rna):
-    # Compare extracted subreads with short-read RNA sequences
     matches = []
     for subread in subreads:
-        if subread in short_read_rna:
+        if subread[0] in short_read_rna:
             matches.append(subread)
     return matches
 
-
-# converts subreads to fasta file to run short read (STAR/HISAT2/Minimap2) RNA seq on
 def subreads_to_fasta(subreads, output_file):
     with open(output_file, 'w') as fasta_file:
-        for i, subread in enumerate(subreads):
-            # Write the header line with a unique identifier
-            fasta_file.write(f">subread_{i + 1}\n")
-            # Write the sequence data
+        for subread, start, end, ref_start, ref_end, long_read_id, subread_id in subreads:
+            fasta_file.write(f">{subread_id}\n")
             fasta_file.write(f"{subread}\n")
 
+def subreads_to_bam(subreads, output_file):
+    temp_bam_file = output_file + ".temp.bam"
+    with pysam.AlignmentFile(temp_bam_file, "wb", header=bamfile.header) as bam_out:
+        for i, (subread, start, end, ref_start, ref_end, long_read_id, subread_id) in enumerate(subreads):
+            read = pysam.AlignedSegment(bamfile.header)  # Correct class name
+            read.query_name = subread_id
+            read.query_sequence = subread
+            read.query_qualities = None
+            read.reference_start = ref_start if ref_start is not None else start  # Using ref_start if available
+            read.flag = 0
+            read.cigar = [(0, len(subread))]
+            bam_out.write(read)
 
-def compare_cigar_strings(long_read_cigar, subread_cigars):
-    match_count = 0
-    total_subreads = len(subread_cigars)
+    os.rename(temp_bam_file, output_file)
 
-    for subread_cigar in subread_cigars:
-        if subread_cigar == long_read_cigar:
-            match_count += 1
-
-    precision = match_count / total_subreads
-    accuracy = match_count / 1  # Assuming 1 long read
-
-    return precision, accuracy
-
-
-# This function tests all the functions written above and prints out their results
 def testFunctions(givenBamfile):
-    parseCigarArray = parseCigar(givenBamfile)  # calling parseCigar
-    seqs = parseCigarArray[0]  # getting the sequences of all the reads
-    cigrs = parseCigarArray[1]  # getting the CIGAR strings of all the reads
-    SoftorHard = parseCigarArray[2][0]  # whether the left clip is soft or hard
+    parseCigarArray = parseCigar(givenBamfile)
+    seqs = parseCigarArray[0]
+    SoHArray = parseCigarArray[2]
+    SoH = SoHArray[0]
+    QRtable2Array = QRtable2(givenBamfile, SoH)
 
-    # Use BAM filename directly in function call
-    QRtable2Array = QRtable2(givenBamfile, SoftorHard)
-
-    # first dataset
     correctData = QRtable1(givenBamfile)
     for x in correctData:
         print(x)
@@ -234,53 +140,40 @@ def testFunctions(givenBamfile):
     reference_positions2 = QRtable2Array[1]
     splices = QRtable2Array[2]
 
-    # second dataset that also contains the positions of splices within the sequences
     print(QRtable2Array[3])
 
-    # Minimap2 long read example sequence to run
-    read_rna = seqs[0]
+    all_subreads = []  # Collecting subreads for all long reads
+    for read_id, read_seq in seqs:
+        print(f"Processing long read ID: {read_id}")
+        length_of_subread = 150
+        subreads = extract_subreads(read_seq, query_positions2, length_of_subread, read_id, reference_positions2)
 
-    # Extracts our appropriate subreads
-    length_of_subread = 150
-    subreads = extract_subreads(seqs[0], query_positions2, length_of_subread)
+        print(f"Subreads from {read_id}:")
+        for subread, start, end, ref_start, ref_end, long_read_id, subread_id in subreads:
+            print(f"Subread ID: {subread_id}, Subread: {subread}, Start: {start}, End: {end}")
 
-    matches = compare_subreads(subreads, read_rna)
-    print("Subreads:", matches)
+        matches = compare_subreads(subreads, read_seq)
+        print("Matched Subreads:", matches)
 
-    # Outputs the subreads in a FASTA file format for short read analysis
-    output_file1 = 'new_subreads.fasta'
+        all_subreads.extend(subreads)  # Collect subreads from all reads
+
     output_file2 = "/Users/AlvinZhang2026/bio_data/new_subreads.fasta"
-    subreads_to_fasta(matches, output_file2)
+    subreads_to_fasta(all_subreads, output_file2)
 
-    # Random extraction and comparison of subreads
-    subreads2 = extract_random_subreads(seqs[0], length_of_subread, query_positions2, reference_positions2)
+    output_file_bam = "/Users/AlvinZhang2026/bio_data/new_subreads.bam"
+    subreads_to_bam(all_subreads, output_file_bam)
+
+    # Generating random subreads for demonstration purposes
     print("Random Subreads:")
-    for i, (subread, start_pos, end_pos, base_start, ref_start, ref_end) in enumerate(subreads2):
-        print(
-            f"Subread {i + 1}: {subread} (Query Start: {start_pos}, Query End: {end_pos}, Ref Start: {ref_start}, Ref End: {ref_end}, Base Start: {base_start})")
+    for i, (subread, start_pos, end_pos, ref_start, ref_end, long_read_id, subread_id) in enumerate(all_subreads):
+        subread_id = f"{long_read_id}_random_subread_{i + 1}_start_{start_pos}_end_{end_pos}"
+        print(f"Subread ID: {subread_id}, Subread {i + 1}: {subread} (Query Start: {start_pos}, Query End: {end_pos}, Ref Start: {ref_start}, Ref End: {ref_end})")
 
     output_file3 = "/Users/AlvinZhang2026/bio_data/random_subreads.fasta"
-    subreads_to_fasta([subread for subread, _, _, _, _, _ in subreads2], output_file3)
+    subreads_to_fasta([(subread, start, end, ref_start, ref_end, long_read_id, f"{long_read_id}_random_subread_{i + 1}_start_{start}_end_{end}") for i, (subread, start, end, ref_start, ref_end, long_read_id, subread_id) in enumerate(all_subreads)], output_file3)
 
-    # Create a mock AlignedSegment object for the long read
-    long_read_mock = pysam.AlignedSegment()
-    long_read_mock.cigarstring = cigrs[0]  # Assuming the first CIGAR string corresponds to the long read
-
-    # Create mock AlignedSegment objects for the subreads
-    subread_mocks = []
-    for subread in subreads:
-        mock = pysam.AlignedSegment()
-        mock.query_sequence = subread
-        mock.cigarstring = cigrs[0]  # Use the same CIGAR string for simplicity
-        subread_mocks.append(mock)
-
-    precision, accuracy = compare_cigar_strings(long_read_mock.cigarstring, [mock.cigarstring for mock in subread_mocks])
-
-    print(f"Precision: {precision}")
-    print(f"Accuracy: {accuracy}")
-
-
-# run testFunctions
+# Run the test function
 testFunctions(bamfile)
 
+# Close the BAM file
 bamfile.close()
